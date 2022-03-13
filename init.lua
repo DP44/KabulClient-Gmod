f513cd4020bac0d693f5e88fc469635afe6daa1a
DP44 <38116775+DP44@users.noreply.github.com>
2022-03-13 08:21:40 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,4 +7,4 @@ pS.g_pLocalPlayer:ConCommand('gmod_mcore_test 1; mat_queue_mode -1; cl_threaded_
 -- Propkill settings.
 pS.g_pLocalPlayer:ConCommand('cl_interp_all 0; cl_cmdrate 1000; cl_updaterate 1000; cl_interp 0; cl_interp_ratio 0; cl_drawspawneffect 0; rate 750000')
 
-chat.AddText(pS.g_pDefaultColor, '[KuteHook]', color_white, ' KuteHook v2.3 Public Edition loaded.')
+chat.AddText(pS.g_pDefaultColor, '[KabulClient]', color_white, ' KabulClient v2.3 Public Edition loaded.')
